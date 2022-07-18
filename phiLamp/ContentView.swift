c90cb0b112c3f7796b940f03f97c4ee9361cd2dd
alex_ezhovv <aezhov4@gmail.com>
2022-07-18 20:42:13 +0800

    Initial Commit

---
+++ phiLamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  phiLamp
+//
+//  Created by Артём Рябенко on 18.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ phiLamp/phiLampApp.swift
@@ -0,0 +1,17 @@
+//
+//  phiLampApp.swift
+//  phiLamp
+//
+//  Created by Артём Рябенко on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct phiLampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
